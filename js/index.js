fb955fd2dbb848a0217656fdd5bac4ee67b4d6d5
sabila111 <sharminsharker2002@gmail.com>
2023-08-22 21:55:48 +0600

    added the js file

--- js/index.js
+++ js/index.js
@@ -10,4 +10,17 @@ function kitchenSpace(clickedElement) {
     const price = parentDiv .childNodes[7].innerText;
     total = parseInt(total) + parseInt(price);
      document.getElementById('total').innerText= total;
+
+     const sellButton = document.getElementById('sellButton');
+     if (total >= 200) {
+        sellButton.classList.remove('disabled');
+    } else {
+        sellButton.classList.add('disabled');
+    }
 }
+
+// Initialize the button's status on page load
+document.addEventListener('DOMContentLoaded', function() {
+    const sellButton = document.getElementById('sellButton');
+    sellButton.classList.add('disabled');
+});
